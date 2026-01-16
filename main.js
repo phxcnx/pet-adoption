55f360580334fe436637f7948061cc2d6c8da3d7
phxcnx <phxcnx@gmail.com>
2026-01-16 14:34:52 +0700

    Pull in real dynamic pet data

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const template = document.querySelector("#pet-card-template")
 const wrapper = document.createDocumentFragment()
 
 async function petsArea() {
-    const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
+    const petsPromise = await fetch("https://phxcnx-netlify-pets.netlify.app/.netlify/functions/pets")
     const petsData = await petsPromise.json()
     petsData.forEach(pet => {
         const clone = template.content.cloneNode(true)
